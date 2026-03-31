28ccc1cf089caa49e390585bfb16fadd51f32db3
jatingarg <jatingarg2596@gmail.com>
2026-03-31 21:58:34 +0530

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import warnings
-warnings.filterwarnings("ignore")
-
-from scanner.tls_scanner import scan_subdomain
-
-result = scan_subdomain("digi2wl.pnb.bank.in")
-for key, value in result.items():
-    print(f"{key}: {value}")
